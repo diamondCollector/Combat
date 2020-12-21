f048755624aae9b9b97601684851bf5329236d6e
IG <igor.widawski@gmail.com>
2020-12-21 20:34:01 +0100

    Combat system

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour, ICombat
+{
+    [SerializeField] float healthPoints;
+
+    public void DealDamage(float inDamage)
+    {
+        
+    }
+
+    public void TakeDamage(float inDamage)
+    {
+        healthPoints -= inDamage;
+        Debug.Log(this.name + " health: " + healthPoints);
+    }
+}
---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    [SerializeField] float _maxDistance;
+    [SerializeField] float _maxAngle;
+    [SerializeField] float damage;
+    List<Enemy> _currentEnemies;
+
+    private void Start()
+    {
+        _currentEnemies = new List<Enemy>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            for (int i = 0; i < _currentEnemies.Count; i++)
+            {
+                if (Vector3.Distance(transform.position, _currentEnemies[i].transform.position) > _maxDistance ||
+                    Vector3.Angle(transform.position, _currentEnemies[i].transform.position) > _maxAngle)
+                {
+                    _currentEnemies.RemoveAt(i);
+                } 
+                else
+                {
+                    _currentEnemies[i].TakeDamage(damage);
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var enemy = other.GetComponent<Enemy>();
+
+        if (enemy != null && !_currentEnemies.Contains(enemy))
+        {
+            _currentEnemies.Add(enemy);
+        }
+    }
+}
