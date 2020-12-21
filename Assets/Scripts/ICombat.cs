0eb4298726a2bab9e98b884f5cc78bf0e8efc08c
IG <igor.widawski@gmail.com>
2020-12-21 20:36:41 +0100

    Combat system upload

---
+++ Assets/Scripts/ICombat.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ICombat
+{
+    void TakeDamage(float inDamage);
+    void DealDamage(float inDamage);
+}
---
+++ Assets/Scripts/Patrolling.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrolling : MonoBehaviour
+{
+    [SerializeField] Transform[] _waypoints;
+    Transform _currentWaypoint;
+    int _waypointsCounter;
+    [SerializeField] float stoppingDuration;
+    [SerializeField] float _speed;
+
+    void Start()
+    {
+        _currentWaypoint = _waypoints[_waypointsCounter % _waypoints.Length];
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, _currentWaypoint.position, _speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, _currentWaypoint.position) < 0.1)
+        {
+            StartCoroutine(ChangeWaypoint(stoppingDuration));                        
+        }
+    }
+
+    IEnumerator ChangeWaypoint(float stoppingDuration)
+    {
+        _waypointsCounter++;
+        yield return new WaitForSeconds(stoppingDuration);       
+        _currentWaypoint = _waypoints[_waypointsCounter % _waypoints.Length];
+    }
+}
