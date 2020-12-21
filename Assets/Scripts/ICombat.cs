3b19427f28c5d4f70114807b832ae8a8dba1e3f4
IG <igor.widawski@gmail.com>
2020-12-21 20:35:51 +0100

    Reset

--- Assets/Scripts/ICombat.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface ICombat
-{
-    void TakeDamage(float inDamage);
-    void DealDamage(float inDamage);
-}
